927c431dc4eed985b8567b5a49dfe4ff0c8e573a
Tarun <ytarun3755@gmail.com>
2024-08-05 12:11:13 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {Navbar} from './Components/Navbar/navbar';
+
+function App() {
+  return (
+    <div>
+       <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Navbar.css"
+
+export const navbar = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
